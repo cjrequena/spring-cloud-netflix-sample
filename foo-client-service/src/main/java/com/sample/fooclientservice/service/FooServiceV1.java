a5d6bd8c3b7d559f3e692092dae89335c5e821f5
Carlos Requena <requenacj@gmail.com>
2019-11-14 14:47:20 +0100

    misc

--- foo-client-service/src/main/java/com/sample/fooclientservice/service/FooServiceV1.java
+++ foo-client-service/src/main/java/com/sample/fooclientservice/service/FooServiceV1.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.json.JsonMergePatch;
@@ -285,7 +286,9 @@ public class FooServiceV1 {
   /**
    *
    */
-  @FeignClient(name = "foo-server-service", url = "http://localhost:9080/foo-server-service")
+  //@FeignClient(name = "foo-server-service", url = "http://localhost:9080/foo-server-service")
+  @FeignClient(name = "foo-server-service")
+  @RequestMapping(value = "/foo-server-service")
   public interface FooServerServiceV1Feign {
 
     /**
